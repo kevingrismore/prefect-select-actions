3a0cd6ad893868d71387a36485ea803d82e04477
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2023-11-13 16:52:07 -0500

    add params to prefect.yaml

--- flows/my_flow.py
+++ flows/my_flow.py
@@ -2,9 +2,9 @@ from prefect import flow
 
 
 @flow(log_prints=True)
-def my_flow():
-    print("Hello world!")
+def my_flow(name):
+    print(f"Hello {name}!")
 
 
 if __name__ == "__main__":
-    my_flow()
+    my_flow("Kevin")
